fc63a23bbb7f74290f0411f259af6aa2e77a1b59
nikhilbachani <nikhilbachani25@gmail.com>
2020-10-12 23:32:32 -0400

    Implemented code to display game from input string

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+    static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         /* Stage 1: Welcome to the battlefield! */
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("O X O");
+        // System.out.println("X O X");
+        // System.out.println("O X O");
+        // System.out.println("O X O");
+
+        /* Stage 2: The user is the game master */
+        String cells = scanner.nextLine();
+        System.out.println("---------");
+        System.out.println("| " + cells.charAt(0) + " " + cells.charAt(1) + " " + cells.charAt(2) + " |");
+        System.out.println("| " + cells.charAt(3) + " " + cells.charAt(4) + " " + cells.charAt(5) + " |");
+        System.out.println("| " + cells.charAt(6) + " " + cells.charAt(7) + " " + cells.charAt(8) + " |");
+        System.out.println("---------");
     }
 }
